4c58b5de5eff1db64d03f2ca9df5258057d777fe
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-10-09 03:38:48 +0500

    The Art of Web Development: My Portfolio

---
+++ script.js
@@ -0,0 +1,81 @@
+function myMenuFunction() {
+    let menuBtn = document.getElementById("myNavMenu");
+    if (menuBtn.className === "nav-menu") {
+        menuBtn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+
+const body = document.querySelector("body");
+const toggleSwitch = document.getElementById("toggle-switch");
+
+toggleSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+});
+
+
+let typingEffect = new Typed(".typed-text", {
+    strings: ["Designer", "Software Engineer", "Programmer", "Developer", "Coder"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000
+})
+
+
+sr.reveal(".featured-box", {})
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".text-info", { delay: 200 });
+sr.reveal(".text-btn", { delay: 200 });
+sr.reveal(".social-icon ", { delay: 200 });
+sr.reveal(".featured-image", { delay: 200 });
+
+sr.reveal(".project-box", { interval: 200 });
+sr.reveal(".top-header", {});
+
+const srLeft = ScrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true
+
+});
+srLeft.reveal(".about-info", { delay: 100 });
+srLeft.reveal(".contact-info", { delay: 100 });
+
+const srRight = ScrollReveal({
+    origin: "right",
+    distance: "80px",
+    duration: 2000,
+    reset: true
+
+});
+
+
+srRight.reveal(".skill", { delay: 100 });
+srRight.reveal(".skill-box", { delay: 100 });
+srRight.reveal(".form-control", { delay: 100 });
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute("id")
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add("active-link")
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+
+
+
+    })
+}
+window.addEventListener("scroll", scrollActive)
